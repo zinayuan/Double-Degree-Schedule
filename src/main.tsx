c813cab346606911492253aaac8efa1fec135098
zinayuan <zinayuan@gmail.com>
2026-02-06 10:41:49 +0800

    Update main.tsxFix: 使用简化版App确保页面正常显示

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.simple';
 import ErrorBoundary from './ErrorBoundary';
 import './index.css';
 
